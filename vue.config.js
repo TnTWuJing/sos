383ecdacfb0a0d8595ded80e276d657db07ccb6a
TnTWuJing <2256492882@qq.com>
2022-09-20 18:40:42 +0800

    feat(custom): 添加了 配置端口，axios, elemt-ui

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    // 端口号
+    port:666,
+    // 主机名
+  host:"localhost",
+//  启动项目时是否自动打开浏览器
+open:true,
+// 是否启动
+https:false
+  }
 })
